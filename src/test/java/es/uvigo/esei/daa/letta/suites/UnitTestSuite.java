f572a25c465391891d12ac1ad220b46c16665129
yrlorenzo <yrlorenzo@esei.uvigo.es>
2016-04-07 16:51:38 +0200

    Corregido EventUnitTest: Agregados tests de set null en y null start

--- src/test/java/es/uvigo/esei/daa/letta/suites/UnitTestSuite.java
+++ src/test/java/es/uvigo/esei/daa/letta/suites/UnitTestSuite.java
@@ -1,15 +1,12 @@
 package es.uvigo.esei.daa.letta.suites;
 
-import es.uvigo.esei.daa.letta.entities.UserUnitTest;
 import es.uvigo.esei.daa.letta.entities.EventUnitTest;
+import es.uvigo.esei.daa.letta.entities.UserUnitTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import static org.junit.runners.Suite.SuiteClasses;
 
-/**
- * Created by yago on 6/04/16.
- */
 @SuiteClasses({
     UserUnitTest.class,
     EventUnitTest.class
