13fe31ea770a496d4b892ed1fc612dacf3b7ab06
yrlorenzo <yrlorenzo@esei.uvigo.es>
2016-04-07 18:07:27 +0200

    Agregado dao JavaScrip para la vista Creado y finalizado dao de events y users de la vista en JavaScript

--- src/main/java/es/uvigo/esei/daa/letta/LettaApplication.java
+++ src/main/java/es/uvigo/esei/daa/letta/LettaApplication.java
@@ -3,6 +3,7 @@ package es.uvigo.esei.daa.letta;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import es.uvigo.esei.daa.letta.rest.EventResource;
 import es.uvigo.esei.daa.letta.rest.UsersResource;
 
 import java.util.Collections;
@@ -15,7 +16,10 @@ import java.util.stream.Stream;
 public class LettaApplication extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
-		return Stream.of(UsersResource.class)
+		return Stream.of(
+				UsersResource.class,
+				EventResource.class
+		)
 			.collect(Collectors.toSet());
 	}
 	
