6583cae537f78c914c750f9bf8fc3b32db868e2e
yago <y_rodrigez@hotmail.com>
2016-04-14 18:52:37 +0200

    Se ha arreglado los listados principales Ahora se usan las funciones correspondientes a eventos populares

--- src/main/webapp/js/dao/events.js
+++ src/main/webapp/js/dao/events.js
@@ -56,3 +56,31 @@ function deleteEvent(id, done, fail, always) {
         .fail(fail)
         .always(always);
 }
+
+function listPopularEvents(done, fail, always) {
+    done = typeof done !== 'undefined' ? done : function() {};
+    fail = typeof fail !== 'undefined' ? fail : function() {};
+    always = typeof always !== 'undefined' ? always : function() {};
+
+    $.ajax({
+            url: 'rest/events?type=popular',
+            type: 'GET'
+    })
+        .done(done)
+        .fail(fail)
+        .always(always);
+}
+
+function listFeaturedEvents(done, fail, always) {
+    done = typeof done !== 'undefined' ? done : function() {};
+    fail = typeof fail !== 'undefined' ? fail : function() {};
+    always = typeof always !== 'undefined' ? always : function() {};
+
+    $.ajax({
+            url: 'rest/events?type=featured',
+            type: 'GET'
+        })
+        .done(done)
+        .fail(fail)
+        .always(always);
+}
--- src/main/webapp/js/view/events.js
+++ src/main/webapp/js/view/events.js
@@ -3,17 +3,19 @@ function createEventThumbnail(event){
     var asistencia = asistenciaPorcentaje(event);
     return '<div class="col-sm-6 col-md-4">\
     <div class="thumbnail">\
-    <img class = "letta-image-sizer" src="http://www.grey-hare.co.uk/wp-content/uploads/2012/09/Event-management.png" alt="un evento">\
+    <object class = "letta-image-sizer" data="rest/events/'+event.id+'/image" type="image/png">\
+        <img class = "letta-image-sizer" src="http://www.returnofkings.com/wp-content/uploads/2014/12/clown.jpg" alt="un evento">\
+    </object>\
     <div class="caption">\
     <h3>'+ event.title +'</h3>\
     <p>'+ event.description +'</p>\
-    <p>Aforo:\
+    <p>Aforo máximo: '+event.num_assistants+'<br>Completado:\
     <div class="progress">\
         <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"\
     aria-valuemin="0" aria-valuemax="100" style="width:'+asistencia+'">\
         </div>\
         </div>\
-        '+ asistencia +'\
+        '+ asistencia +'%\
         </p>\
         <p><a href="#" class="btn btn-primary" role="button">Asistir</a> <a href="#" class="btn btn-default" role="button">Ver mas</a></p>\
     </div>\
@@ -25,19 +27,18 @@ function asistenciaPorcentaje(event){
     return 0;
 }
 
-function addNearestEvent(event){
+function addPopularEvent(event){
    $('#proximos-eventos').append(createEventThumbnail(event));
-
 }
 
 function initEvents(){
     $.getScript('js/dao/events.js', function(){
-        listEvents(function (events) {
+        listPopularEvents ( function (events) {
             $.each(events, function (key, event) {
-                addNearestEvent(event);
+                addPopularEvent(event);
             });
         }, function(){
-            alertify.error('Error listando eventos');
+            alertify.error('Error listando eventos populares');
         });
     });
 }
