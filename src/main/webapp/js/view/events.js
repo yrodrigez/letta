4b14342b6b1ad5a89dea9c7c44979640b6206ac8
Telos <javygr@gmail.com>
2016-04-18 22:06:08 +0200

    Corrección de la visualización de la búsqueda Corregido el apilamiento de búsquedas, ahora se reinicia cada vez que se vusca de nuevo

--- src/main/webapp/js/view/events.js
+++ src/main/webapp/js/view/events.js
@@ -43,6 +43,7 @@ function showSearch(){
 	var text = formSearchtoText();
     $.getScript('js/dao/events.js', function(){
         searchEvents(text,function (events) {
+            document.getElementById("#searchResult").innerHTML="";
             $.each(events, function (key, event) {
             	addSearch(event);
             });
