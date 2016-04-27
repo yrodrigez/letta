fafd7d585de89c695a59edeec9bbb075f5b1bc6e
yago <y_rodrigez@hotmail.com>
2016-04-27 18:42:41 +0200

    Mejoras en la vista: Ahora los botones del thumbnail (ver más y asistir) aparecen encima de la imágen; Aún no se ha solucionado el problema del padding "extraño" al lado derecho de la página.

--- src/main/webapp/js/view/events.js
+++ src/main/webapp/js/view/events.js
@@ -2,27 +2,23 @@
 function createEventThumbnail(event){
     var asistencia = asistenciaPorcentaje(event);
     return '<div class="col-sm-6 col-md-4">\
-    <div class="letta-event-thumbnail thumbnail">\
-    <object class = "letta-image-sizer" data="rest/events/'+event.id+'/image" type="image/png">\
-        <img class = "letta-image-sizer" src="img/logo.png" alt="un evento">\
-    </object>\
-    <div class="caption">\
-    <h3>'+ event.title +'</h3>\
-    <p>'+ event.description +'</p>\
-    <p>Aforo máximo: '+event.num_assistants+'<br>Completado:\
-    <div class="progress">\
-        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"\
-    aria-valuemin="0" aria-valuemax="100" style="width:'+asistencia+'">\
+        <div class="letta-event-thumbnail thumbnail">\
+           <div class="letta-thumbnail-buttons-container">\
+                <object class = "letta-image-sizer" data="rest/events/'+event.id+'/image" type="image/png">\
+                    <img class = "letta-image-sizer" src="img/logo.png" alt="un evento"/>\
+                </object>\
+                <div class="overlay">\
+                    <a href="#" class="btn btn-primary" role="button">Asistir</a>\
+                    <a href="#" class="btn btn-default" role="button">Ver mas</a>\
+                </div>\
+           </div>\
+           <div class="caption">\
+                <h3>'+ event.title +'</h3>\
+                <p>'+ event.description +'</p>\
+                <p>Aforo máximo: '+event.num_assistants+'\
+                <br/>Completado:'+ asistencia +'% </p>\
+           </div>\
         </div>\
-        </div>\
-        '+ asistencia +'%\
-        </p>\
-        <div class="letta-thumbnail-buttons-container">\
-            <a href="#" class="btn btn-primary" role="button">Asistir</a>\
-            <a href="#" class="btn btn-default" role="button">Ver mas</a>\
-        </div>\
-    </div>\
-    </div>\
     </div>';
 }
 
