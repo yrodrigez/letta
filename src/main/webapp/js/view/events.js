8af798a1ae2155ba0ab50b99c170eed0802ad14e
csnunhez2 <csnunhez2@esei.uvigo.es>
2016-04-27 18:36:59 +0200

    Interfaz Busqueda
    Se ha modificado la interfaz para mostrar los resultados de la busqueda, queda pendiente modificar la forma en que se muestra cada uno de los eventos del resultado

--- src/main/webapp/js/view/events.js
+++ src/main/webapp/js/view/events.js
@@ -35,19 +35,18 @@ function addPopularEvent(event){
 }
 
 function addSearch(event){
-    $('#searchResult').append(createEventThumbnail(event));
+    $('#main').append(createEventThumbnail(event));
 }
 
 function formSearchtoText(){
-    var form = $('#busqueda');
-    return form.find('input[name="buscar"]').val();
+    return $('#buscar').val();
 }
 
 function showSearch(){
     var text = formSearchtoText();
     $.getScript('js/dao/events.js', function(){
         searchEvents(text,function (events) {
-            document.getElementById("searchResult").innerHTML="";
+            document.getElementById("main").innerHTML="";
             $.each(events, function (key, event) {
                 addSearch(event);
             });
@@ -69,3 +68,10 @@ function initEvents(){
         });
     });
 }
+
+$('#buscar').keypress(function(e) {
+    if (e.keyCode == '13') {
+       e.preventDefault();
+       showSearch();
+     }
+  });
